6c534e1c187150e8248a8748005f16baa94029ff
pmw57 <paul.m.wilkins@gmail.com>
2019-03-06 20:32:41 +1300

    Mario acceleration, and animates while running.

--- public/js/traits/Go.js
+++ public/js/traits/Go.js
@@ -3,18 +3,32 @@ import trait from "../Trait.js";
 function makeGo() {
     const go = trait("go");
     go.dir = 0;
-    go.speed = 6000;
+    go.acceleration = 400;
+    go.deceleration = 300;
+    go.dragFactor = 1 / 5000;
+
     go.distance = 0;
     go.heading = 1;
 
     go.update = function update(entity, deltaTime) {
-        entity.vel.x = go.speed * go.dir * deltaTime;
-        if (go.dir) {
+        const absX = Math.abs(entity.vel.x);
+        if (go.dir !== 0) {
+            entity.vel.x += go.acceleration * deltaTime * go.dir;
             go.heading = go.dir;
-            go.distance += Math.abs(entity.vel.x) * deltaTime;
+        } else if (entity.vel.x !== 0) {
+            const decel = Math.min(absX, go.deceleration * deltaTime);
+            entity.vel.x += (
+                entity.vel.x > 0
+                ? -decel
+                : decel
+            );
         } else {
             go.distance = 0;
         }
+        const drag = go.dragFactor * entity.vel.x * absX;
+        entity.vel.x -= drag;
+
+        go.distance += absX * deltaTime;
     };
     return go;
 }
